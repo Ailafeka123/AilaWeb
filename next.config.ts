db04d39f8c4be7b0f1d7765f1b2c25353cf998b4
Ailafeka <ailafeka@gmail.com>
2025-06-28 17:19:23 +0800

    test:測試vercel部屬的部分
    1.增加了navbar.tsx配合style測試module.scss是否正常
    2.目前注意到metadata會因Navbar移動到body內部，測試是否上傳後正常
    3.測試public/icon的渲染是否可以成功
    4.測試vercel是否會在github push之後進行自動CI/CD

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
+// const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
+  output: 'export',
+  basePath:'',
+  assetPrefix:'',
+  publicRuntimeConfig:{
+    basePath:""
+  },
+  // basePath: isProd? '/aila-web':'',
+  // assetPrefix: isProd? '/aila-web':'',
+  // publicRuntimeConfig:{
+  //   basePath:isProd? '/aila-web':''
+  // },
+  images:{
+    unoptimized:true,
+  }
 };
 
 export default nextConfig;
---
+++ src/component/navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Style from '@/style/navbar.module.scss'
+
+export default function Navbar(){
+
+    return (
+        <header className={`${Style.header}`}>
+            
+        </header>
+    );
+
+}
